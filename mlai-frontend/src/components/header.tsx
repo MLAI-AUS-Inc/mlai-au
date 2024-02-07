af01e25509bf9b4580f8da0348bf4ddbb4dd862a
LukasWesemann <luk.wesemann@gmail.com>
2024-02-07 14:23:41 +1100

    Floating menu

--- mlai-frontend/src/components/header.tsx
+++ mlai-frontend/src/components/header.tsx
@@ -7,13 +7,13 @@ const navigation = [
     { name: 'About', href: '/about' },
     { name: 'Events', href: '#' },
     { name: 'Volunteer', href: '#' },
-]
+];
 
 export default function CTA() {
-    const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
+    const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
     return (
-        <header className={`fixed inset-x-0 top-0 z-50 bg-opacity-60 backdrop-filter backdrop-blur-lg`}>
-            <nav className="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
+        <header className="fixed inset-x-0 top-0 z-50 bg-black bg-opacity-60 backdrop-filter backdrop-blur-lg">
+            <nav className="flex items-center justify-between p-3 lg:px-5" aria-label="Global">
                 <div className="flex lg:flex-1">
                     <a href="#" className="-m-1.5 p-1.5">
                         <span className="sr-only">MLAI - Empowering the Australian AI Community</span>
@@ -27,7 +27,7 @@ export default function CTA() {
                 <div className="flex lg:hidden">
                     <button
                         type="button"
-                        className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-400"
+                        className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-white"
                         onClick={() => setMobileMenuOpen(true)}
                     >
                         <span className="sr-only">Open main menu</span>
@@ -36,20 +36,20 @@ export default function CTA() {
                 </div>
                 <div className="hidden lg:flex lg:gap-x-12">
                     {navigation.map((item) => (
-                        <a key={item.name} href={item.href} className="text-sm font-semibold leading-6 text-white text-shadow">
+                        <a key={item.name} href={item.href} className="text-sm font-semibold leading-6 text-white">
                             {item.name}
                         </a>
                     ))}
                 </div>
                 <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-                    <a href="#" className="text-sm font-semibold leading-6 text-white text-shadow">
+                    <a href="#" className="text-sm font-semibold leading-6 text-white">
                         Join us <span aria-hidden="true">&rarr;</span>
                     </a>
                 </div>
             </nav>
             <Dialog open={mobileMenuOpen} onClose={setMobileMenuOpen}>
                 <div className="fixed inset-0 z-50 lg:hidden" />
-                <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+                <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-black px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
                     <div className="flex items-center justify-between">
                         <a href="#" className="-m-1.5 p-1.5">
                             <span className="sr-only">MLAI - Empowering the Australian AI Community</span>
@@ -61,7 +61,7 @@ export default function CTA() {
                         </a>
                         <button
                             type="button"
-                            className="-m-2.5 rounded-md p-2.5 text-gray-700"
+                            className="-m-2.5 rounded-md p-2.5 text-white"
                             onClick={() => setMobileMenuOpen(false)}
                         >
                             <span className="sr-only">Close menu</span>
@@ -69,30 +69,20 @@ export default function CTA() {
                         </button>
                     </div>
                     <div className="mt-6 flow-root">
-                        <div className="-my-6 divide-y divide-gray-500/10">
-                            <div className="space-y-2 py-6">
-                                {navigation.map((item) => (
-                                    <a
-                                        key={item.name}
-                                        href={item.href}
-                                        className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                                    >
-                                        {item.name}
-                                    </a>
-                                ))}
-                            </div>
-                            <div className="py-6">
+                        <div className="-my-6 divide-y divide-gray-200">
+                            {navigation.map((item) => (
                                 <a
-                                    href="#"
-                                    className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                                    key={item.name}
+                                    href={item.href}
+                                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-white hover:bg-gray-700"
                                 >
-                                    Join us
+                                    {item.name}
                                 </a>
-                            </div>
+                            ))}
                         </div>
                     </div>
                 </Dialog.Panel>
             </Dialog>
         </header>
-    )
+    );
 }
