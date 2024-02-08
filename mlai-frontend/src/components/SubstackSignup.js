8a1a86e7b4892fcb449f05faa9d27a88604c237a
LukasWesemann <luk.wesemann@gmail.com>
2024-02-08 12:14:20 +1100

    events section 4col
    4 col events section, hover effects on cards, card size adjustment

--- mlai-frontend/src/components/SubstackSignup.js
+++ mlai-frontend/src/components/SubstackSignup.js
@@ -6,7 +6,7 @@ const SubstackSignup = () => {
       src="https://mlaiaus.substack.com/embed" 
       width="100%" 
       height="150" 
-      style={{ border: '0px', background: 'red' }} 
+      style={{ border: '0px', background: 'white' }} 
       frameBorder="0" 
       scrolling="no">
     </iframe>
