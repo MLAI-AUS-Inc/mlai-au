429356a1fee5c86f2e8f53acaeb3b7f280d8c171
Sam <samwise.donegan@gmail.com>
2024-02-10 13:32:25 +1100

    Added Logrocket and google
    - Added tracking
    - Logrocket
    - Google analytics
    - Missing facebook pixel

--- mlai-frontend/src/app/layout.tsx
+++ mlai-frontend/src/app/layout.tsx
@@ -11,6 +11,9 @@ import {
   ServerIcon,
 } from '@heroicons/react/20/solid'
 import Header from "@/components/header";
+import LogRocket from 'logrocket';
+import Script from "next/script";
+LogRocket.init('4s6cqz/mlai-website');
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -130,6 +133,22 @@ export default function RootLayout({
 
   return (
     <html lang="en">
+      <Script
+      strategy="afterInteractive"
+      src={`https://www.googletagmanager.com/gtag/js?id=G-HVT5DXY0RF}`}
+    />
+    <Script
+      id="google-analytics"
+      strategy="afterInteractive"
+      dangerouslySetInnerHTML={{
+        __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-HVT5DXY0RF');
+          `,
+      }}
+    />
       <body className={inter.className}>
         <Header></Header>
         {children}
