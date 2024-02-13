f71dc944c8b77735f84ae7d05a50f5b7564384e8
Sam <samwise.donegan@gmail.com>
2024-02-13 21:51:09 +1100

    Design updates
     - Dark theme
    - Battery behind hero
    - Teal more pronounced
    - Halo effect on header to make it look like the battery is shining in the dark

--- mlai-frontend/src/app/hackathon/components/ExtendedInfo.tsx
+++ mlai-frontend/src/app/hackathon/components/ExtendedInfo.tsx
@@ -6,7 +6,7 @@ import { Button } from './Button'
 
 export function ExtendedInfo() {
   return (
-    <section id="extendedinfo" aria-label="Schedule" className="py-20 sm:py-16 bg-white">
+    <section id="extendedinfo" aria-label="Schedule" className="py-20 sm:py-16 bg-gray-900">
         <div
           className="absolute inset-x-0 -top-20 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
           aria-hidden="true"
@@ -21,16 +21,16 @@ export function ExtendedInfo() {
         </div>
       <Container className="relative z-10">
         <div className="mx-auto max-w-2xl lg:mx-0 lg:max-w-4xl lg:pr-24">
-          <h2 className="font-display text-4xl font-medium tracking-tighter text-teal-600 sm:text-5xl">
+          <h2 className="font-display text-4xl font-medium tracking-tighter text-teal-500 sm:text-5xl">
             Why an AI Hackathon on Energy in Australia?
           </h2>
-          <p className="mt-4 pb-6 pt-6 font-display text-2xl tracking-tight text-teal-900">
+          <p className="mt-4 pb-6 pt-6 font-display text-2xl tracking-tight text-teal-500">
           Renewables are key to Australias journey to carbon net-zero, but do not produce electricity in a predictable, reliable fashion. This is known to make maintaining a reliable flow of electricity more difficult. 
           </p>
-          <p className="mt-4 pb-6 font-display text-2xl tracking-tight text-teal-900">
+          <p className="mt-4 pb-6 font-display text-2xl tracking-tight text-teal-500">
           Energy storage is increasingly seen as a solution to this issue. However there is some research indicating that energy storage entities potentially disrupt the electricity market, especially if their charging and discharging tactics are driven by profit rather than social welfare.
           </p>
-          <p className="mt-4 pb-0  font-display text-2xl tracking-tight text-teal-900">
+          <p className="mt-4 pb-0  font-display text-2xl tracking-tight text-teal-500">
           The hackathon presents a beginner-friendly challenge in artificial intelligence, inviting participants to manage a virtual battery within the spot market to maximize earnings. This simulation aims to shed light on the potential for individual battery systems to influence or manipulate the Australian electricity market. </p>
         </div>
       
--- mlai-frontend/src/app/hackathon/components/Hero.tsx
+++ mlai-frontend/src/app/hackathon/components/Hero.tsx
@@ -1,51 +1,75 @@
 
 import AnimatedBackground from './AnimatedBackground';
-import { BackgroundImage } from './BackgroundImage';
+// import { BackgroundImage } from './BackgroundImage';
 import { Button } from './Button'
 import { Container } from './Container'
-import  GetTickets  from './GetTickets'
+import GetTickets from './GetTickets'
+import styles from '../AnimatedBackground.module.css';
 
 export function Hero() {
     return (
-        <div className="relative sm:pt-24">
-            <BackgroundImage className="-bottom-14 -top-36" />
+        <div className="relative sm:pt-24 bg-gray-900 min-h-96">
+
+            {/* <BackgroundImage className="-bottom-14 -top-36" /> */}
             <Container className="relative">
-                <div className="mx-auto max-w-2xl lg:max-w-4xl lg:px-12">
-                    <h1 className="font-display text-5xl font-bold tracking-tighter text-teal-600 sm:text-7xl">
-                        MLAI Green Battery Hackathon
-                    </h1>
-                    <div className="mt-6 space-y-6 font-display text-2xl tracking-tight text-teal-600">
+                <div className="relative isolate pt-14">
+                    <div
+                        className={`${styles.animatedBackground} absolute inset-x-0 -top-40 z-10 transform-gpu blur-3xl sm:-top-80`}
+                        aria-hidden="true"
+                    >
+                        <div
+                            className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#80ffdd] to-[#89b9fc] opacity-20 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
+                            style={{
+                                clipPath:
+                                    'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+                            }}
+                        />
+                    </div>
+
+                </div>
+                <div className="absolute flex justify-center items-center min-w-full -mt-40 mb-10 z-0">
+                    <AnimatedBackground />
+                </div>
+                <div className='absolute flex justify-center items-center w-full'>
+                    <div className="mx-auto max-w-2xl pb-32 sm:pb-48 lg:pb-56">
+
+                        <div className="text-center">
+                            <h1 className="text-7xl font-bold tracking-tight text-black sm:text-9xl">
+                                MLAI Green Battery Hack
+                            </h1>
+                        </div>
+                        <div className="mt-6 space-y-6 font-display text-2xl tracking-tight text-black">
                         <p>
                         The MLAI Green Battery Hack is a beginner friendly hackathon where you are given a simulated battery and have to use it to trade the electricity spot market with the help of an AI model. 
                         </p>
                         
-                        <p>
+                        {/* <p>
                         This simulation will give insight into the propensity of private batteries to disrupt or “game” the electricity market. Your solution could inform Australian policy to achieve net zero!
-                        </p>
+                        </p> */}
                         
                     </div>
-                    <Button href="#schedule" className="mt-10 w-full sm:hidden">
-                        Get your tickets
-                    </Button>
-                    <dl className="mt-10 grid grid-cols-2 gap-x-10 gap-y-6 sm:mt-16 sm:gap-x-16 sm:gap-y-10 sm:text-center lg:auto-cols-auto lg:grid-flow-col lg:grid-cols-none lg:justify-start lg:text-left z-10">
-                        {[
-                            ['Speakers', '12'],
-                            ['People Attending', '250'],
-                            ['Location', 'Melbourne & Sydney'],
-                        ].map(([name, value]) => (
-                            <div key={name}>
-                                <dt className="font-mono text-sm text-teal-600">{name}</dt>
-                                <dd className="mt-0.5 text-2xl font-semibold tracking-tight text-teal-900">
-                                    {value}
-                                </dd>
-                            </div>
-                        ))}
-                    </dl>
+                        <Button href="#schedule" className="mt-10 w-full sm:hidden">
+                            Get your tickets
+                        </Button>
+                        <dl className="mt-10 grid grid-cols-2 gap-x-10 gap-y-6 sm:mt-16 sm:gap-x-16 sm:gap-y-10 sm:text-center lg:auto-cols-auto lg:grid-flow-col lg:grid-cols-none lg:justify-start lg:text-left z-10">
+                            {[
+                                ['Speakers', '12'],
+                                ['People Attending', '250'],
+                                ['Location', 'Melbourne & Sydney'],
+                            ].map(([name, value]) => (
+                                <div key={name}>
+                                    <dt className="font-mono text-sm text-black">{name}</dt>
+                                    <dd className="mt-0.5 text-2xl font-semibold tracking-tight text-teal-900">
+                                        {value}
+                                    </dd>
+                                </div>
+                            ))}
+                        </dl>
+                    </div>
                 </div>
-                <div className="flex justify-center items-center max-h-96 mb-10 z-0 -ml-10">
-                <AnimatedBackground />
-            </div>
-            </Container>
-        </div>
+                <div className="relative isolate min-h-96">
+                </div>
+            </Container >
+        </div >
     )
 }
