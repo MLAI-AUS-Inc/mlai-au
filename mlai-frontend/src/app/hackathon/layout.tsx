06bd4197af6641840178514a4e1395a702877e96
Sam <samwise.donegan@gmail.com>
2024-02-21 09:43:52 +1100

    Updated meta tags and OG
    - Updated meta tags
    - Added Open Graph tags
    - Added robots.txt

--- mlai-frontend/src/app/hackathon/layout.tsx
+++ mlai-frontend/src/app/hackathon/layout.tsx
@@ -1,4 +1,27 @@
+import { Metadata } from "next";
 import Layout from "./components/Layout"
+
+export const metadata: Metadata = {
+  title: "MLAI Green Battery Hack",
+  description: "The MLAI Green Battery Hack is a beginner friendly AI hackathon to support Australia's journey to carbon net zero. Your AI model will control a simulated battery trading in the electricity spot market.",
+  openGraph: {
+    title: 'MLAI',
+    description: "The MLAI Green Battery Hack is a beginner friendly AI hackathon to support Australia's journey to carbon net zero. Your AI model will control a simulated battery trading in the electricity spot market.",
+    url: 'https://mlai.au/hackathon',
+    type: 'website',
+    images: [
+      {
+        url: 'photos/gbh_melbourne.webp',
+        width: 1200,
+        height: 630,
+        alt: 'A Giant green battery in the middle of Melbourne and Sydney',
+      },
+    ],
+  },
+  robots: {
+    index: true, // Allow indexing of the page by search engines
+  },
+};
 
 
 export default function HackathonLayout({
