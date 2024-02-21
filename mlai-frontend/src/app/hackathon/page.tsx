85d8544428c790aef0cb978001d3a972ec66e90f
Sam <samwise.donegan@gmail.com>
2024-02-21 14:16:40 +1100

    Still trying
    - Updated all npm packages as the metadata should be auto updating given the project structure

--- mlai-frontend/src/app/hackathon/page.tsx
+++ mlai-frontend/src/app/hackathon/page.tsx
@@ -6,45 +6,6 @@ import { InfoForHackers } from './components/InfoForHackers'
 import { ExtendedInfo } from './components/ExtendedInfo'
 import { GbhPeople } from './components/gbhPeople'
 import { MentorsVolunteers } from './components/MentorsVolunteers'
-import { Metadata } from 'next'
-
-export const metadata: Metadata = {
-    metadataBase: new URL("https://mlai.au/hackathon"), 
-    title: "MLAI Green Battery Hack",
-    description: "MLAI Green Battery Hackathon, Melbourne & Sydney, April-May 2024",
-    authors: [
-        {
-          name: 'Dr Sam Donegan',
-          url: 'https://www.linkedin.com/in/samueldonegan/', 
-        },
-        {
-            name: 'Dr Lukas Wesemann',
-            url: 'https://www.linkedin.com/in/lukaswesemann/', 
-          },
-      ],
-    openGraph: {
-      title: 'MLAI',
-      description: "MLAI Green Battery Hackathon, Melbourne & Sydney, April-May 2024",
-      url: 'https://mlai.au/hackathon',
-      type: 'website',
-      images: [
-        {
-            url: '/MLAI-Logo.png',
-            width: 1200,
-            height: 630,
-            alt: 'MLAI Logo, a Kangaroo wearing sunglasses',
-          },
-      ],
-    },
-    robots: {
-      index: true, // Allow indexing of the page by search engines
-      follow: false, // Do not follow the links on the page to discourage scrapers
-      noarchive: true,
-      nosnippet: true,
-      noimageindex: true,
-      nocache: true,
-    },
-  };
 
 export default function HackathonPage() {
     return (
