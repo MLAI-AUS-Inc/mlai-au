876d65ce96c0397d928b9da2ca92b020ef81ee8a
BWOil <152672793+BenjapornW@users.noreply.github.com>
2024-03-01 23:28:37 +1100

    reused newsletter component

--- mlai-frontend/src/app/about/page.tsx
+++ mlai-frontend/src/app/about/page.tsx
@@ -1,6 +1,7 @@
 import CTA from "@/components/CTA";
 import Events from "@/components/events";
 import Hero from "@/components/hero";
+import Newsletter from "@/components/newsletter";
 import Team from "@/components/team";
 
 const stats = [
@@ -86,11 +87,7 @@ export default function About() {
                 </a>
               </div>
             </div>
-            <img
-              src="https://images.unsplash.com/photo-1567532900872-f4e906cbf06a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1280&q=80"
-              alt=""
-              className="mt-10 aspect-[6/5] w-full max-w-lg rounded-2xl object-cover sm:mt-16 lg:mt-0 lg:max-w-none xl:row-span-2 xl:row-end-2 xl:mt-36"
-            />
+           
           </div>
         </div>
         <div className="absolute inset-x-0 bottom-0 -z-10 h-24 bg-gradient-to-t from-white sm:h-32" />
@@ -138,44 +135,9 @@ export default function About() {
       </div>
    
     <Events></Events>
-
-    <div className="bg-white py-16 sm:py-24 lg:py-32">
-      <div className="mx-auto grid max-w-7xl grid-cols-1 gap-10 px-6 lg:grid-cols-12 lg:gap-8 lg:px-8">
-        <div className="max-w-xl text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl lg:col-span-7">
-          <h2 className="inline sm:block lg:inline xl:block">Stay up to date with MLAI</h2>{' '}
-        </div>
-        <form className="w-full max-w-md lg:col-span-5 lg:pt-2">
-          <div className="flex gap-x-4">
-            <label htmlFor="email-address" className="sr-only">
-              Email address
-            </label>
-            <input
-              id="email-address"
-              name="email"
-              type="email"
-              autoComplete="email"
-              required
-              className="min-w-0 flex-auto rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              placeholder="Enter your email"
-            />
-            <button
-              type="submit"
-              className="flex-none rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-            >
-              Subscribe
-            </button>
-          </div>
-          <p className="mt-4 text-sm leading-6 text-gray-900">
-            We care about your data. Read our{' '}
-            <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
-              privacy&nbsp;policy
-            </a>
-            .
-          </p>
-        </form>
-      </div>
-    </div>
+    <Newsletter></Newsletter>
     <CTA></CTA>
+
   
   </div>
   
