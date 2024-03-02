762a184df88b36fa591d51710de19a209277c728
BWOil <152672793+BenjapornW@users.noreply.github.com>
2024-03-02 15:37:56 +1100

    deleted button

--- mlai-frontend/src/app/about/page.tsx
+++ mlai-frontend/src/app/about/page.tsx
@@ -73,17 +73,6 @@ export default function About() {
               Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet
               fugiat veniam occaecat fugiat aliqua.
             </p>
-            <div className="mt-10 flex items-center gap-x-6">
-              <a
-                href="#"
-                className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-              >
-                Get started
-              </a>
-              <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
-                Learn more →
-              </a>
-            </div>
           </div>
 
           {/* Values Section */}
