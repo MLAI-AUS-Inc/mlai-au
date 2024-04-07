cd74be64ed6a0f925ec9b483ff564db330c4933c
Sam <samwise.donegan@gmail.com>
2024-04-07 12:08:11 +1000

    Bug fixing
    - vercel cache so leaderboard updates
    - added JSON download

--- mlai-frontend/src/app/hackathon/components/Leaderboard.tsx
+++ mlai-frontend/src/app/hackathon/components/Leaderboard.tsx
@@ -123,7 +123,7 @@ export const Leaderboard: React.FC<LeaderboardProps> = ({ topScores = [] }) => {
                 </h2>
 
                 <div className="bg-gray-900 py-10">
-                    <h2 className="px-4 text-base font-semibold leading-7 text-white sm:px-6 lg:px-8">Best performing commit</h2>
+                    <h2 className="px-4 text-base font-semibold leading-7 text-white sm:px-6 lg:px-8">Most recent commit</h2>
                     <div className="overflow-x-auto">
                         <table className="mt-6 w-full whitespace-nowrap text-left">
                             <colgroup>
