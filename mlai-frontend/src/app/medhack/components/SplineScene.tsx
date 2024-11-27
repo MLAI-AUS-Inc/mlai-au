0b87847955f7c5cdc8f9988b1e68b53202c79f79
Sam <samwise.donegan@gmail.com>
2024-11-27 14:28:37 +1100

    spline scene on page

---
+++ mlai-frontend/src/app/medhack/components/SplineScene.tsx
@@ -0,0 +1,11 @@
+import Spline from '@splinetool/react-spline/next';
+
+export default function SplineScene() {
+  return (
+    <main>
+      <Spline
+        scene="https://prod.spline.design/01IEWP9jTop4g8OC/scene.splinecode" 
+      />
+    </main>
+  );
+}
--- mlai-frontend/src/app/medhack/page.tsx
+++ mlai-frontend/src/app/medhack/page.tsx
@@ -1,11 +1,12 @@
 import { Hero } from './components/Hero'
+import SplineScene from './components/SplineScene'
 
 
 export default function Home() {
   return (
     <>
       <Hero />
-
+      <SplineScene />
     </>
   )
 }
