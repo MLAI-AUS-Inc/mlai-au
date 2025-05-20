e39a10f838d10412eee55288abda01906213670c
LukasWesemann <hi@mlai.au>
2025-05-20 21:16:07 +1000

    grant updates

--- mlai-frontend/src/components/footer.tsx
+++ mlai-frontend/src/components/footer.tsx
@@ -166,7 +166,7 @@ export default function Events() {
             ))}
           </div>
           <p className="mt-8 text-xs leading-5 text-gray-300 md:order-1 md:mt-0">
-            &copy; 2024 MLAI Aus Inc. All rights reserved.
+            &copy; 2025 MLAI Aus Inc. All rights reserved.
           </p>
         </div>
       </div>
