039b1a23304fc965c59ddb86f3c192da7ebe755f
Dr Sam Donegan <samwise.donegan@gmail.com>
2026-01-21 19:16:41 +1100

    feat(content): add SEO config for why-is-artificial-intelligence-bad

--- app/articles/seo-config.ts
+++ app/articles/seo-config.ts
@@ -29,6 +29,13 @@ export type ArticleSeoConfig = {
 }
 
 export const BASE_ARTICLE_SEO_CONFIG: Record<string, ArticleSeoConfig> = {
+    '/articles/featured/why-is-artificial-intelligence-bad': {
+        toc: true,
+        howTo: false,
+        mediaObject: false,
+        citations: true,
+        internalLinks: [],
+    },
     '/articles/featured/what-are-artificial-intelligence': {
         toc: true,
         howTo: false,
