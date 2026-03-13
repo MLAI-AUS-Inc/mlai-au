5747fb83d3543797adbef4af729a62a82035ad3a
Dr Sam Donegan <samwise.donegan@gmail.com>
2026-03-13 15:52:18 +1100

    feat(content): add SEO config for the-non-technical-founder-s-guide-to-finding-a-technical-co-founder

--- app/articles/seo-config.ts
+++ app/articles/seo-config.ts
@@ -29,6 +29,13 @@ export type ArticleSeoConfig = {
 }
 
 export const BASE_ARTICLE_SEO_CONFIG: Record<string, ArticleSeoConfig> = {
+    '/articles/featured/the-non-technical-founder-s-guide-to-finding-a-technical-co-founder': {
+        toc: true,
+        howTo: false,
+        mediaObject: false,
+        citations: true,
+        internalLinks: [],
+    },
     '/articles/featured/how-technology-affects-education-negatively': {
         toc: true,
         howTo: false,
