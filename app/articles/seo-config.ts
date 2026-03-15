e97007d7227acce1e4adebdd5fd39ea6b8821c1a
Dr Sam Donegan <samwise.donegan@gmail.com>
2026-03-15 18:29:02 +1100

    feat(content): add SEO config for how-to-find-a-sales-led-non-technical-founder-to-be-your-ceo

--- app/articles/seo-config.ts
+++ app/articles/seo-config.ts
@@ -29,6 +29,13 @@ export type ArticleSeoConfig = {
 }
 
 export const BASE_ARTICLE_SEO_CONFIG: Record<string, ArticleSeoConfig> = {
+    '/articles/featured/how-to-find-a-sales-led-non-technical-founder-to-be-your-ceo': {
+        toc: true,
+        howTo: false,
+        mediaObject: false,
+        citations: true,
+        internalLinks: [],
+    },
     '/articles/featured/how-to-test-for-a-cofounder-values-match-before-you-commit': {
         toc: true,
         howTo: false,
