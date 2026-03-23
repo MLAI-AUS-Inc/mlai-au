92ef76730d50c0aaa742a07af6021333ea6f96d2
Dr Sam Donegan <samwise.donegan@gmail.com>
2026-03-23 15:11:52 +1100

    feat(content): add SEO config for how-long-has-artificial-intelligence-truly-been-around

--- app/articles/seo-config.ts
+++ app/articles/seo-config.ts
@@ -29,6 +29,13 @@ export type ArticleSeoConfig = {
 }
 
 export const BASE_ARTICLE_SEO_CONFIG: Record<string, ArticleSeoConfig> = {
+    '/articles/featured/how-long-has-artificial-intelligence-truly-been-around': {
+        toc: true,
+        howTo: false,
+        mediaObject: false,
+        citations: true,
+        internalLinks: [],
+    },
     '/articles/featured/a-practical-guide-on-how-to-create-an-artificial-intelligence': {
         toc: true,
         howTo: false,
