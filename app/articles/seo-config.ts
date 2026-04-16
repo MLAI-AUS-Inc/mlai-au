52fe0f8f615c00dc3f1cbe6c646c6272b4eddf27
Content Factory <content-factory-bot@users.noreply.github.com>
2026-04-16 01:04:27 +0000

    feat(content): update what-is-quantum-artificial-intelligence-and-why-it-matters

--- app/articles/seo-config.ts
+++ app/articles/seo-config.ts
@@ -29,6 +29,13 @@ export type ArticleSeoConfig = {
 }
 
 export const BASE_ARTICLE_SEO_CONFIG: Record<string, ArticleSeoConfig> = {
+    '/articles/featured/what-is-quantum-artificial-intelligence-and-why-it-matters': {
+        toc: true,
+        howTo: false,
+        mediaObject: false,
+        citations: true,
+        internalLinks: [],
+    },
     '/articles/featured/how-to-startup-a-small-business-in-australia': {
         toc: true,
         howTo: false,
