ed81d60828f2aea76c0cfaeb43ceac408615603d
Content Factory <content-factory-bot@users.noreply.github.com>
2026-07-26 09:25:15 +0000

    feat(content): update what-community-is-in-ai-and-why-it-is-more-than-a-group

--- app/articles/seo-config.ts
+++ app/articles/seo-config.ts
@@ -29,6 +29,13 @@ export type ArticleSeoConfig = {
 }
 
 export const BASE_ARTICLE_SEO_CONFIG: Record<string, ArticleSeoConfig> = {
+    '/articles/featured/what-community-is-in-ai-and-why-it-is-more-than-a-group': {
+        toc: true,
+        howTo: false,
+        mediaObject: false,
+        citations: true,
+        internalLinks: [],
+    },
     '/articles/featured/what-is-an-accelerator-and-is-it-right-for-your-ai-startup': {
         toc: true,
         howTo: false,
